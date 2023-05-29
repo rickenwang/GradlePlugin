bea5e402ce46c82d2d0b659f1aeed94312eb1d7f
wangkang <wangkang14313@cvte.com>
2023-05-29 11:40:09 +0800

    [bugfix] 问题描述 [jira] none [inf] none

---
+++ app/src/main/java/com/demo/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.demo.test;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * <p>
+ * Copyright (c) 2010-2020 Tencent Cloud. All rights reserved.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
